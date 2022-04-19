7fd3e03c37f0986f459a4fd9a7405933de65d487
Mojem33 <85997191+Mojem33@users.noreply.github.com>
2022-04-19 10:30:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-conts box = document.querySe
+const box = document.querySelector(".box")
+
+const observer = new ResizeObserver(entries =>{
+   const boxElement = entries[0]
+   const isSmall = boxElement.contentRect.width < 150
+   boxElement.target.style.backgroundColor = isSmall ?"#013220" : "BACKGROUND1.png"
+})
+
+observer.observe(box)
